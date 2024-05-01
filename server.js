4021c2f34fa18f5985775738c3e050fad1a60cb3
Echkalov Nikita <echkalov@e-krit.ru>
2024-05-01 16:21:17 +0300

    master — add auth

--- server.js
+++ server.js
@@ -1,168 +1,177 @@
-import log from 'book';
-import Koa from 'koa';
-import tldjs from 'tldjs';
-import Debug from 'debug';
-import http from 'http';
-import { hri } from 'human-readable-ids';
-import Router from 'koa-router';
+import Koa from "koa"
+import tldjs from "tldjs"
+import Debug from "debug"
+import http from "http"
+import { hri } from "human-readable-ids"
+import Router from "koa-router"
 
-import ClientManager from './lib/ClientManager';
+import ClientManager from "./lib/ClientManager"
 
-const debug = Debug('localtunnel:server');
+const debug = Debug("localtunnel:server")
 
-export default function(opt) {
-    opt = opt || {};
+export default function (opt) {
+  opt = opt || {}
 
-    const validHosts = (opt.domain) ? [opt.domain] : undefined;
-    const myTldjs = tldjs.fromUserSettings({ validHosts });
-    const landingPage = opt.landing || 'https://localtunnel.github.io/www/';
+  const validHosts = opt.domain ? [opt.domain] : undefined
+  const myTldjs = tldjs.fromUserSettings({ validHosts })
+  const landingPage = opt.landing || "https://localtunnel.github.io/www/"
 
-    function GetClientIdFromHostname(hostname) {
-        return myTldjs.getSubdomain(hostname);
+  function GetClientIdFromHostname(hostname) {
+    return myTldjs.getSubdomain(hostname)
+  }
+
+  const manager = new ClientManager(opt)
+
+  const schema = opt.secure ? "https" : "http"
+
+  const app = new Koa()
+  const router = new Router()
+
+  router.get("/api/status", async (ctx, next) => {
+    const stats = manager.stats
+    ctx.body = {
+      tunnels: stats.tunnels,
+      mem: process.memoryUsage()
+    }
+  })
+
+  router.get("/api/tunnels/:id/status", async (ctx, next) => {
+    const clientId = ctx.params.id
+    const client = manager.getClient(clientId)
+    if (!client) {
+      ctx.throw(404)
+      return
     }
 
-    const manager = new ClientManager(opt);
+    const stats = client.stats()
+    ctx.body = {
+      connected_sockets: stats.connectedSockets
+    }
+  })
 
-    const schema = opt.secure ? 'https' : 'http';
+  app.use(router.routes())
+  app.use(router.allowedMethods())
 
-    const app = new Koa();
-    const router = new Router();
+  // root endpoint
+  app.use(async (ctx, next) => {
+    const path = ctx.request.path
 
-    router.get('/api/status', async (ctx, next) => {
-        const stats = manager.stats;
-        ctx.body = {
-            tunnels: stats.tunnels,
-            mem: process.memoryUsage(),
-        };
-    });
+    // skip anything not on the root path
+    if (path !== "/") {
+      await next()
+      return
+    }
 
-    router.get('/api/tunnels/:id/status', async (ctx, next) => {
-        const clientId = ctx.params.id;
-        const client = manager.getClient(clientId);
-        if (!client) {
-            ctx.throw(404);
-            return;
-        }
+    if (opt.token && opt.token !== ctx.headers.authorization) {
+      res.statusCode = 403
+      res.end(403)
+    }
 
-        const stats = client.stats();
-        ctx.body = {
-            connected_sockets: stats.connectedSockets,
-        };
-    });
+    const isNewClientRequest = ctx.query["new"] !== undefined
+    if (isNewClientRequest) {
+      const reqId = hri.random()
+      debug("making new client with id %s", reqId)
+      const info = await manager.newClient(reqId)
 
-    app.use(router.routes());
-    app.use(router.allowedMethods());
+      const url = schema + "://" + info.id + "." + ctx.request.host
+      info.url = url
+      ctx.body = info
+      return
+    }
 
-    // root endpoint
-    app.use(async (ctx, next) => {
-        const path = ctx.request.path;
+    // no new client request, send to landing page
+    ctx.redirect(landingPage)
+  })
 
-        // skip anything not on the root path
-        if (path !== '/') {
-            await next();
-            return;
-        }
+  // anything after the / path is a request for a specific client name
+  // This is a backwards compat feature
+  app.use(async (ctx, next) => {
+    const parts = ctx.request.path.split("/")
 
-        const isNewClientRequest = ctx.query['new'] !== undefined;
-        if (isNewClientRequest) {
-            const reqId = hri.random();
-            debug('making new client with id %s', reqId);
-            const info = await manager.newClient(reqId);
+    // any request with several layers of paths is not allowed
+    // rejects /foo/bar
+    // allow /foo
+    if (parts.length !== 2) {
+      await next()
+      return
+    }
 
-            const url = schema + '://' + info.id + '.' + ctx.request.host;
-            info.url = url;
-            ctx.body = info;
-            return;
-        }
+    const reqId = parts[1]
 
-        // no new client request, send to landing page
-        ctx.redirect(landingPage);
-    });
+    // limit requested hostnames to 63 characters
+    if (!/^(?:[a-z0-9][a-z0-9\-]{4,63}[a-z0-9]|[a-z0-9]{4,63})$/.test(reqId)) {
+      const msg = "Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters."
+      ctx.status = 403
+      ctx.body = {
+        message: msg
+      }
+      return
+    }
 
-    // anything after the / path is a request for a specific client name
-    // This is a backwards compat feature
-    app.use(async (ctx, next) => {
-        const parts = ctx.request.path.split('/');
+    if (opt.token && opt.token !== ctx.headers.authorization) {
+      res.statusCode = 403
+      res.end(403)
+    }
 
-        // any request with several layers of paths is not allowed
-        // rejects /foo/bar
-        // allow /foo
-        if (parts.length !== 2) {
-            await next();
-            return;
-        }
+    debug("making new client with id %s", reqId)
+    const info = await manager.newClient(reqId)
 
-        const reqId = parts[1];
+    const url = schema + "://" + info.id + "." + ctx.request.host
+    info.url = url
+    ctx.body = info
+    return
+  })
 
-        // limit requested hostnames to 63 characters
-        if (! /^(?:[a-z0-9][a-z0-9\-]{4,63}[a-z0-9]|[a-z0-9]{4,63})$/.test(reqId)) {
-            const msg = 'Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters.';
-            ctx.status = 403;
-            ctx.body = {
-                message: msg,
-            };
-            return;
-        }
+  const server = http.createServer()
 
-        debug('making new client with id %s', reqId);
-        const info = await manager.newClient(reqId);
+  const appCallback = app.callback()
 
-        const url = schema + '://' + info.id + '.' + ctx.request.host;
-        info.url = url;
-        ctx.body = info;
-        return;
-    });
+  server.on("request", (req, res) => {
+    // without a hostname, we won't know who the request is for
+    const hostname = req.headers.host
+    if (!hostname) {
+      res.statusCode = 400
+      res.end("Host header is required")
+      return
+    }
 
-    const server = http.createServer();
+    const clientId = GetClientIdFromHostname(hostname)
+    if (!clientId) {
+      appCallback(req, res)
+      return
+    }
 
-    const appCallback = app.callback();
+    const client = manager.getClient(clientId)
+    if (!client) {
+      res.statusCode = 404
+      res.end("404")
+      return
+    }
 
-    server.on('request', (req, res) => {
-        // without a hostname, we won't know who the request is for
-        const hostname = req.headers.host;
-        if (!hostname) {
-            res.statusCode = 400;
-            res.end('Host header is required');
-            return;
-        }
+    client.handleRequest(req, res)
+  })
 
-        const clientId = GetClientIdFromHostname(hostname);
-        if (!clientId) {
-            appCallback(req, res);
-            return;
-        }
+  server.on("upgrade", (req, socket, head) => {
+    const hostname = req.headers.host
+    if (!hostname) {
+      socket.destroy()
+      return
+    }
 
-        const client = manager.getClient(clientId);
-        if (!client) {
-            res.statusCode = 404;
-            res.end('404');
-            return;
-        }
+    const clientId = GetClientIdFromHostname(hostname)
+    if (!clientId) {
+      socket.destroy()
+      return
+    }
 
-        client.handleRequest(req, res);
-    });
+    const client = manager.getClient(clientId)
+    if (!client) {
+      socket.destroy()
+      return
+    }
 
-    server.on('upgrade', (req, socket, head) => {
-        const hostname = req.headers.host;
-        if (!hostname) {
-            socket.destroy();
-            return;
-        }
+    client.handleUpgrade(req, socket)
+  })
 
-        const clientId = GetClientIdFromHostname(hostname);
-        if (!clientId) {
-            socket.destroy();
-            return;
-        }
-
-        const client = manager.getClient(clientId);
-        if (!client) {
-            socket.destroy();
-            return;
-        }
-
-        client.handleUpgrade(req, socket);
-    });
-
-    return server;
-};
+  return server
+}
